a991861730b33da4e1987dfd45502a9bde768df2
Nick Escalona <nicholas.escalona@gmail.com>
2022-08-24 13:53:30 -0500

    add client app

---
+++ ClientApp/Program.cs
@@ -0,0 +1,28 @@
+﻿const string serverUrlVariable = "SERVER_URL";
+const int delayMs = 3000;
+
+Console.WriteLine("Starting client");
+
+if (Environment.GetEnvironmentVariable(serverUrlVariable) is string serverUrl)
+{
+    using const HttpClient httpClient = new();
+
+    while (true)
+    {
+        using const HttpRequestMessage request = new(HttpMethod.Get, serverUrl);
+
+        using const HttpResponseMessage response = await httpClient.SendAsync(request);
+        Console.WriteLine(response);
+
+        string content = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(content);
+        Console.WriteLine();
+
+        await Task.Delay(delayMs);
+    }
+}
+else
+{
+    Console.Error.WriteLine($"Could not find environment variable {serverUrlVariable}, exiting");
+    return 1;
+}
